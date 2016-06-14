93fc9df8dd17be13aa101b9d0178fe3f6a8ad0f7
Tiago Floriano <tiagofloriano@gmail.com>
2016-06-14 16:49:55 -0300

    Estrutura inicial do módulo Criador.

---
+++ pages/criador.php
@@ -0,0 +1,52 @@
+        <div class="">
+            <div class="page-title">
+              <div class="title_left">
+                <h3>Criador</h3>
+              </div>
+
+              <div class="title_right">
+                <div class="col-md-5 col-sm-5 col-xs-12 form-group pull-right top_search">
+                  <div class="input-group">
+                    <input type="text" class="form-control" placeholder="Pesquisar formul&aacute;rios">
+                    <span class="input-group-btn">
+                      <button class="btn btn-default" type="button">Buscar!</button>
+                    </span>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="clearfix"></div>
+
+<?
+if($_GET["sp"] == "new"){
+?>
+            <div class="row">
+              <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2>Novo formul&aacute;rio</h2>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
+                      Add content to the page ...
+                  </div>
+                </div>
+              </div>
+            </div>
+<? }else{ ?>
+            <div class="row">
+              <div class="col-md-12 col-sm-12 col-xs-12">
+                <div class="x_panel">
+                  <div class="x_title">
+                    <h2>Formul&aacute;rios &nbsp; <a href="?criador&p=forms&sp=new" class="btn btn-success">novo</a></h2>
+                    <div class="clearfix"></div>
+                  </div>
+                  <div class="x_content">
+                      lista de formularios
+                  </div>
+                </div>
+              </div>
+            </div>
+<? } ?>
+          </div>
